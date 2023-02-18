6383ca2523daabf9212b8f9e350f74401b0307a7
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2023-02-18 14:03:16 +0300

    api related configurations

---
+++ city-list-api/city-list-rest/src/main/resources/liquibase/changeset/v0.01-mock.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.budget (id, name) VALUES ('201892fb-96ce-4490-a3ad-016af742191f', 'Budget 1');
+INSERT INTO public.budget (id, name) VALUES ('c04f6dee-5c4d-40cd-a5a0-19ae1fc22c5b', 'Budget 2');
+INSERT INTO public.budget (id, name) VALUES ('6cb8a9b2-7170-4873-bf39-00e3b2589eba', 'Budget 3');
+INSERT INTO public.budget (id, name) VALUES ('b8dfd27e-49a3-487b-8a5c-d96d3b08694b', 'Budget 4');
+INSERT INTO public.budget (id, name) VALUES ('37f59d3e-509c-4da0-a547-4b0c6cd44853', 'Budget 5');
+
