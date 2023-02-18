5b389b199458484e8a2ae1599a6d8256fb2a594b
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2023-02-18 14:00:50 +0300

    maven modules for backend created

---
+++ city-list-api/city-list-rest/src/main/java/com/maemresen/city/list/rest/CityListApplication.java
@@ -0,0 +1,12 @@
+package com.maemresen.city.list.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityListApplication.class, args);
+    }
+}
