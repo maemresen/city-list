63a7f1780cf7d2c8ce0b873597da291fa811640e
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2023-02-22 23:50:53 +0300

    service javadocs completed

--- city-list-api/city-list-domain/src/main/java/com/maemresen/city/list/domain/service/UserService.java
+++ city-list-api/city-list-domain/src/main/java/com/maemresen/city/list/domain/service/UserService.java
@@ -3,7 +3,17 @@ package com.maemresen.city.list.domain.service;
 import com.maemresen.city.list.domain.exception.ServiceException;
 import com.maemresen.city.list.domain.service.model.dto.UserResponseDto;
 
+/**
+ * User related operations
+ */
 public interface UserService {
+
+	/**
+	 * To get user entity of the authenticated user
+	 *
+	 * @return self info of the auth user
+	 * @throws ServiceException something went wrong
+	 */
 	UserResponseDto getSelf() throws ServiceException;
 
 }
