9c15da2dffa65c67db9be9db5f7d9b938d483c9c
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2023-02-23 00:18:02 +0300

    junit mvn config

---
+++ city-list-api/city-list-domain/src/test/java/com/maemresen/city/list/domain/service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package com.maemresen.city.list.domain.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+	@Test
+	void getSelf() {
+		String x = "emre";
+		Assertions.assertEquals("emre2", x);
+	}
+}
