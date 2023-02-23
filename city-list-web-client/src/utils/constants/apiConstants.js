510af8099fe45d341989e297cb428ee8ff72f961
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2023-02-23 05:14:57 +0300

    dockerize environment

--- city-list-web-client/src/utils/constants/apiConstants.js
+++ city-list-web-client/src/utils/constants/apiConstants.js
@@ -1,5 +1,5 @@
 const API_CONSTANTS = Object.freeze({
-  BASE_URL: 'http://localhost:8080/api',
+  BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
 });
 
 export default API_CONSTANTS;
